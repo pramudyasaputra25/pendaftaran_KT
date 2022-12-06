77acd665e3adc25abce9ed18903bc13eb29ccf3b
pramudyasaputra25 <110385363+pramudyasaputra25@users.noreply.github.com>
2022-12-06 16:38:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Website pendaftaran anggota karang taruna">
+    <meta name="author" content="Theokusumaaa">
+    <title>KARANG TARUNA</title>
+    <!-- <link rel="canonical" href="nama-domain"> -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/logo.png" sizes="180x180">
+    <link rel="icon" href="logo.png" sizes="32x32" type="image/png">
+
+    <!-- <meta name="theme-color" content="#563d7c"> -->
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+            }
+        }
+        .nav-link {
+            padding-left: 30px !important;
+        }
+    </style>
+    <!-- Custom styles for this template -->
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow" style="background: white !important; height: 70px;">
+            <div class="container">
+                <a class="navbar-brand" href="#"><b>KARANG TARUNA</b></a>
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <!-- <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">MASUK</a>
+                        </li>
+                    </ul>
+                    <a href="registrasi.php" class="btn btn-danger rounded-pill ml-3">DAFTAR</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main role="main" style="position: relative">
+        <div class="container mt-5">
+            <div class="d-flex justify-content-between pt-5">
+                <div class="col-md-7">
+                    <h2 class="text-dark">PENDAFTARAN ONLINE <br> KARANG TARUNA  <br></h2>
+                    <p class="lead">
+                        Untuk calon pendaftar anggota baru karang taruna bisa mendaftar lewat website ini...
+                    </p>
+                    <button class="btn btn-danger rounded-pill"><a href="registrasi.php" class="text-white">DAFTAR SEKARANG</a></button>
+                </div>
+                <div class="col-md-5">
+                   <img src="logo.png" alt="">
+                </div>
+            </div>
+            <!-- /.row -->
+            <!-- START THE FEATURETTES -->
+            <hr class="featurette-divider">
+            <div class="row featurette">
+
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">KARANG TARUNA <span class="text-muted"></span></h2>
+                    <p class="lead">
+                    Karang Taruna adalah organisasi sosial kemasyarakatan sebagai wadah dan sarana pengembangan setiap anggota masyarakat yang tumbuh dan berkembang atas dasar kesadaran dan tanggung jawab sosial dari, oleh dan untuk masyarakat terutama generasi muda di wilayah desa/kelurahan terutama bergerak dibidang usaha kesejahteraan sosial.
+                    </p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img src="logo.png" alt="Karang Taruna">
+                </div>
+            </div>
+            <hr class="featurette-divider">
+            <div class="row featurette">
+
+                <div class="col-md-7 ">
+                    <h2 class="featurette-heading">SEJARAH KARANG TARUNA <span class="text-muted"></span></h2>
+                    <p class="lead">
+                    Karang Taruna lahir pada tanggal 26 September 1960 di Kampung Melayu Jakarta, melalui proses Experimental Project Karang Taruna, kerjasama masyarakat Kampung Melayu/ Yayasan Perawatan Anak Yatim (YPAY) dengan Jawatan Pekerjaan Sosial/Departemen Sosial. Pembentukan Karang Taruna dilatarbelakangi oleh banyaknya anak-anak yang menyandang masalah sosial antara lain seperti anak yatim, putus sekolah, mencari nafkah membantu orang tua dsb. Masalah tersebut tidak terlepas dari kemiskinan yang dialami sebagian masyarakat kala itu.
+                    </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="logo1.jpg" alt="Karang Taruna">
+                </div>
+            </div>
+            <hr class="featurette-divider">
+            <div class="row featurette">
+
+                <div class="col-md-7 order-md-5 ">
+                    <h2 class="featurette-heading">TUGAS KARANG TARUNA <span class="text-muted"></span></h2>
+                    <p class="lead">
+                    Tugas dari karang taruna bisa dibilang hampir sama dari fungsi karang taruna itu sendiri. Berikut ini beberapa tugas utama dari karang taruna.
+                    <p>1.Membantu meningkatkan usaha ekonomi bagi masyarakat sekitar yang biasanya dapat dilakukan dengan cara menyelenggarakan bazar di waktu-waktu tertentu dan dilaksanakan di lingkungan sekitar karang taruna.</p>
+                    <p>2.Ikut serta dalam memelihara dan menumbuhkan rasa tanggung jawab serta kesadaran bagi setiap masyarakat khususnya para pemuda.</p>
+                    <p>3.Berperan dalam memperkuat persatuan dan kesatuan bagi warga-warga di sekitar organisasi karang taruna sekaligus menumbuhkan semangat kebangsaan.</p>
+                    <p>4.Menjaga tradisi, kebudayaan atau kearifan lokal di wilayah organisasi itu berdiri.</p>
+                    </p>
+                </div>
+                <div class="col-md-5 order-md-2">
+                    <img src="logo2.jpg" alt="Karang Taruna">
+                </div>
+            </div>
+            <!-- /END THE FEATURETTES -->
+        </div>
+        <!-- /.container -->
+        <!-- FOOTER -->
+        <footer class="container text-center mt-5">
+        <p class="float-right">
+        </p>
+        <p>
+            &copy; Copyright 2022 mud ganteng &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a>
+        </p>
+        </footer>
+    </main>
+    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+    <script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script> -->
+
+</body>
+</html>
